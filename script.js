da4038c788a12f147da2ae4d5b96bb0017d46cc0
assam1-svg <assam1@farmingdale.edu>
2026-03-12 23:35:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const budgetPhotos = [
+    "BudgetPicture.png",
+    "signup.png",
+    "budgets.png",
+    "CodeSnippet.png",
+]
+
+const leftButton = document.querySelector("#leftSlide");
+const rightButton = document.querySelector("#rightSlide");
+const budgetPhoto = document.querySelector("#budgetPhoto");
+
+leftButton.addEventListener("click", moveLeft);
+rightButton.addEventListener("click", moveRight);
+let i = 0;
+
+function moveLeft() {
+ 
+    i = i === 0 ? (budgetPhotos.length-1) : i-1;
+    budgetPhoto.setAttribute("src", budgetPhotos[i]);
+}
+
+function moveRight() {
+    console.log("Move right");
+    i = i === (budgetPhotos.length-1) ? 0 : i+1;
+    budgetPhoto.setAttribute("src", budgetPhotos[i]);
+}
